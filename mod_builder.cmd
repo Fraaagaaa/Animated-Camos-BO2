7db88d5c08ee66871e9d051768b8ecd0e1646a3c
Miguel Fraga <miguel.fraga.pico@udc.es>
2026-07-23 10:46:09 +0200

    m14 and an94

---
+++ mod_builder.cmd
@@ -0,0 +1,66 @@
+@echo off
+set MOD_NAME=zm_animatedcamos
+set GAME_FOLDER=C:\Program Files (x86)\Steam\steamapps\common\Call of Duty Black Ops II
+set OAT_BASE=C:\OAT
+set MOD_BASE=%cd%
+
+::Below is an example of assets needed to load in order to build the mod. 
+::Each line is seperated by a "^".
+"%OAT_BASE%\linker.exe" ^
+-v ^
+--load "%GAME_FOLDER%\zone\all\zm_transit.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_prison.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_buried.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_tomb.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_highrise.ff" ^
+--load "%GAME_FOLDER%\zone\all\so_zclassic_zm_transit.ff" ^
+--load "%GAME_FOLDER%\zone\all\so_zsurvival_zm_transit.ff" ^
+--load "%GAME_FOLDER%\zone\all\common_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\ui_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\ui_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_nuked.ff" ^
+--load "%GAME_FOLDER%\zone\all\patch_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc4_load_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\code_post_gfx_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\code_post_gfx_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\code_pre_gfx_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\code_pre_gfx_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\common_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\common_patch_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc0_load_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc1_load_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc1_load_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc2_load_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc2_load_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc3_load_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc3_load_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlc4_load_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\dlczm0_load_zm.ff" ^
+--load "%GAME_FOLDER%\zone\all\patch_mp.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_transit_patch.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_prison_patch.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_buried_patch.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_tomb_patch.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_nuked_patch.ff" ^
+--load "%GAME_FOLDER%\zone\all\zm_highrise_patch.ff" ^
+--base-folder "%OAT_BASE%" ^
+--add-asset-search-path "%MOD_BASE%" ^
+--source-search-path "%MOD_BASE%\zone_source" ^
+--output-folder "%MOD_BASE%\zone" mod
+
+set err=%ERRORLEVEL%
+
+if %err% EQU 0 (
+    if not exist "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%" mkdir "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%"
+
+    XCOPY "%MOD_BASE%\zone\mod.ff" "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%\mod.ff" /Y
+    XCOPY "%MOD_BASE%\mod.json" "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%\mod.json" /Y
+    XCOPY "%MOD_BASE%\mod.iwd" "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%\mod.iwd" /Y
+
+    echo DONE!
+) ELSE (
+    COLOR C
+    echo FAIL!
+    pause
+)
+
---
+++ scripts/zm/_zm_camo_override.gsc
@@ -0,0 +1,71 @@
+#include common_scripts\utility;
+#include maps\mp\_utility;
+#include maps\mp\zombies\_zm_utility;
+#include maps\mp\zombies\_zm_weapons;
+
+init()
+{
+    getplayers()[0] IPrintLn("Animated Camos Loaded!");
+	replaceFunc(getfunction("maps/mp/zombies/_zm_weapons", "get_pack_a_punch_weapon_options"), ::get_pack_a_punch_weapon_options);
+}
+
+get_pack_a_punch_weapon_options( weapon )
+{
+    if ( !isdefined( self.pack_a_punch_weapon_options ) )
+        self.pack_a_punch_weapon_options = [];
+
+    smiley_face_reticle_index = 1;
+    base = get_base_name( weapon );
+
+    camo_index = get_camo_based_on_weapon(weapon);
+
+    lens_index = randomintrange( 0, 6 );
+    reticle_index = randomintrange( 0, 16 );
+    reticle_color_index = randomintrange( 0, 6 );
+    plain_reticle_index = 16;
+    r = randomint( 10 );
+    use_plain = r < 3;
+
+    if ( "saritch_upgraded_zm" == base )
+        reticle_index = smiley_face_reticle_index;
+    else if ( use_plain )
+        reticle_index = plain_reticle_index;
+
+    scary_eyes_reticle_index = 8;
+    purple_reticle_color_index = 3;
+
+    if ( reticle_index == scary_eyes_reticle_index )
+        reticle_color_index = purple_reticle_color_index;
+
+    letter_a_reticle_index = 2;
+    pink_reticle_color_index = 6;
+
+    if ( reticle_index == letter_a_reticle_index )
+        reticle_color_index = pink_reticle_color_index;
+
+    letter_e_reticle_index = 7;
+    green_reticle_color_index = 1;
+
+    if ( reticle_index == letter_e_reticle_index )
+        reticle_color_index = green_reticle_color_index;
+
+    self iprintln("w: " + weapon + " | index: " + camo_index);
+    self.pack_a_punch_weapon_options[weapon] = self calcweaponoptions( camo_index, lens_index, reticle_index, reticle_color_index );
+    return self.pack_a_punch_weapon_options[weapon];
+}
+
+get_camo_based_on_weapon(weapon)
+{
+    if(IsSubStr(weapon, "an94"))
+        return 39;
+    else if(IsSubStr(weapon, "m14"))
+        return 40;
+
+
+
+    if ( "zm_prison" == level.script )
+        return 40;
+    else if ( "zm_tomb" == level.script )
+        return 45;
+    return 39;
+}
